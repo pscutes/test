a0bb4bd318914fd7f243ccd3ea61459943bcc684
Divya Singarapu <Divya.Singarapu_HCL@kantar.com>
2018-11-21 13:07:56 +0530

    first commit

---
+++ Divya/Divya/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Divya
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver;
+            var service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+            ChromeOptions option = new ChromeOptions();
+            option.AddArgument("--headless");
+
+            driver = new ChromeDriver(service, option);
+            //wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl("https://www.wikifolio.com/");
+            driver.Quit();
+        }
+    }
+}
